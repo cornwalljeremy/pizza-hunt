e528e6bcfa4732a89ddc9b5c4432ff6099ceea3b
cornwalljeremy <cornwall.jeremy@gmail.com>
2021-01-06 16:23:18 -0700

    watch for the comma

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,8 +4,14 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
-      .then((dbPizzaData) => res.json(dbPizzaData))
-      .catch((err) => {
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => {
         console.log(err);
         res.status(400).json(err);
       });
